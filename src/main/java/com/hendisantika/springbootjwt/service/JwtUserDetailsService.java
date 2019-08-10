24b15002647d1ea4c5a4e0a6a95b3ea718a85208
Hendi Santika <hendisantika@gmail.com>
2019-08-11 05:14:12 +0700

    #10 - Add JwtUserDetailsService

---
+++ src/main/java/com/hendisantika/springbootjwt/service/JwtUserDetailsService.java
@@ -0,0 +1,49 @@
+package com.hendisantika.springbootjwt.service;
+
+import com.hendisantika.springbootjwt.model.User;
+import com.hendisantika.springbootjwt.model.UserDTO;
+import com.hendisantika.springbootjwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-08-11
+ * Time: 05:12
+ */
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder bcryptEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                new ArrayList<>());
+    }
+
+    public User save(UserDTO user) {
+        User newUser = new User();
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+        return userRepository.save(newUser);
+    }
+}
