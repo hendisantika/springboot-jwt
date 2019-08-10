963467f774a045a9ce5b310ef01f19b403570686
Hendi Santika <hendisantika@gmail.com>
2019-08-11 05:09:47 +0700

    #5 - Add User Model

---
+++ src/main/java/com/hendisantika/springbootjwt/model/User.java
@@ -0,0 +1,45 @@
+package com.hendisantika.springbootjwt.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-08-11
+ * Time: 05:09
+ */
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String username;
+
+    @Column
+    @JsonIgnore
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
