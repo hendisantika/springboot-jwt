c1d2873df9e52132c49ac0c713c4975649b82733
Hendi Santika <hendisantika@gmail.com>
2019-08-11 05:08:12 +0700

    #4 - Add JwtAuthenticationEntryPoint

---
+++ src/main/java/com/hendisantika/springbootjwt/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,32 @@
+package com.hendisantika.springbootjwt.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-08-11
+ * Time: 05:07
+ */
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+    private static final long serialVersionUID = -7858869558953243875L;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
