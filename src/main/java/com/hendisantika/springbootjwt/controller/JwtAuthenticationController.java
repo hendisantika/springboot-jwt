18f234a032cbf207dbab7b6a36ec06e1497a1982
Hendi Santika <hendisantika@gmail.com>
2019-08-11 05:18:08 +0700

    #15 - Add JwtAuthenticationController

---
+++ src/main/java/com/hendisantika/springbootjwt/controller/JwtAuthenticationController.java
@@ -0,0 +1,31 @@
+package com.hendisantika.springbootjwt.controller;
+
+import com.hendisantika.springbootjwt.config.JwtTokenUtil;
+import com.hendisantika.springbootjwt.service.JwtUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-08-11
+ * Time: 05:17
+ */
+@RestController
+@CrossOrigin
+public class JwtAuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
+}
