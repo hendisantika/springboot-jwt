68d02b5b4cac452589b273c355bbdcd4987e587d
Hendi Santika <hendisantika@gmail.com>
2019-08-11 05:19:23 +0700

    #18 - Add createAuthenticationToken method in JwtAuthenticationController

--- src/main/java/com/hendisantika/springbootjwt/controller/JwtAuthenticationController.java
+++ src/main/java/com/hendisantika/springbootjwt/controller/JwtAuthenticationController.java
@@ -1,6 +1,8 @@
 package com.hendisantika.springbootjwt.controller;
 
 import com.hendisantika.springbootjwt.config.JwtTokenUtil;
+import com.hendisantika.springbootjwt.model.JwtRequest;
+import com.hendisantika.springbootjwt.model.JwtResponse;
 import com.hendisantika.springbootjwt.model.UserDTO;
 import com.hendisantika.springbootjwt.service.JwtUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -32,6 +35,18 @@ public class JwtAuthenticationController {
 
     @Autowired
     private JwtUserDetailsService userDetailsService;
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ResponseEntity<?> saveUser(@RequestBody UserDTO user) throws Exception {
